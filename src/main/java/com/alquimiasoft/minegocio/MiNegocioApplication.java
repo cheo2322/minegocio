e459c98636ae078ec2a06e629aef960327084d23
Sergio Hidalgo <barcecheo2319@gmail.com>
2025-04-15 16:08:03 -0500

    First commit

---
+++ src/main/java/com/alquimiasoft/minegocio/MiNegocioApplication.java
@@ -0,0 +1,12 @@
+package com.alquimiasoft.minegocio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiNegocioApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MiNegocioApplication.class, args);
+  }
+}
